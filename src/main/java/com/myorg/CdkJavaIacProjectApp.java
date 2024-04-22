9f7e35a9425c27df3cce429b11c6a20e413a62d5
prabin tripathi <trpthprvn@gmail.com>
2024-04-22 16:48:56 +0530

    my cdk code

--- src/main/java/com/myorg/CdkJavaIacProjectApp.java
+++ src/main/java/com/myorg/CdkJavaIacProjectApp.java
@@ -1,10 +1,8 @@
 package com.myorg;
 
 import software.amazon.awscdk.App;
-import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
 
-import java.util.Arrays;
 
 public class CdkJavaIacProjectApp {
     public static void main(final String[] args) {
